7b6e0254fea24dbef2b57ae9c1818a09a52ac150
doodleman218 <dcwolff218@gmail.com>
2020-06-17 22:18:20 -0500

    css work

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,70 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 export class home extends Component {
   render() {
     return (
-      <div>
-        Welcome Home
-      </div>
-    )
+     <html>
+     <body className="Home">
+        <Link
+          to="/ZorasDomain"
+          style={{ color: "black", textDecoration: "none" }}
+        >
+          <div className="zora-container">
+            <a className="card-title">Zoras Domain</a> <br></br>
+            <a>
+              Quests:
+              {
+                this.props.quests.filter(
+                  (questObj) => questObj.location === "Zoras Domain"
+                ).length
+              }
+            </a>
+          </div>
+        </Link>
+        <Link
+          to="/HyruleCastle"
+          style={{ color: "black", textDecoration: "none" }}
+        >
+          <div className="castle-container">
+            <a className="card-title">Hyrule Castle</a>
+            <br></br>
+            <a>
+              Quests:
+              {
+                this.props.quests.filter(
+                  (questObj) => questObj.location === "Hyrule Castle"
+                ).length
+              }
+            </a>
+          </div>
+        </Link>
+        <Link
+          to="/GerudoDesert"
+          style={{ color: "black", textDecoration: "none" }}
+        >
+          <div className="gerudo-container">
+            <a className="card-title">Gerudo Desert</a>
+            <br></br>
+            <a>
+              Quests:
+              {
+                this.props.quests.filter(
+                  (questObj) => questObj.location === "Gerudo Desert"
+                ).length
+              }
+            </a>
+          </div>
+        </Link>
+      </body>
+      </html>
+    );
   }
 }
 
-export default home
+const mapStateToProps = (state) => {
+  return { quests: state.quests };
+};
+
+export default connect(mapStateToProps)(home);
